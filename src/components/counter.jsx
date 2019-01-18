81fa0996d9283d4c4077eda04028a9ffef9d4d0f
motchang <motchang@gmail.com>
2019-01-18 10:27:10 +0900

    Done

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  styles = {
+    fontWeight: "bold"
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    const x = <span>Zero</span>;
+    return value === 0 ? x : value;
+  }
+
+  render() {
+    console.log("props", this.props);
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
